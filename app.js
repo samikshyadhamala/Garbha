9c9ac18b69c90da7fc5cbb06750cccf2484e61ef
Unika Ghimire <ghimire.unika505@gmail.com>
2025-10-12 16:03:30 +0545

    added deletion/reset account handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const documentRoutes = require("./routes/documentRoutes");
 const weightRoutes = require("./routes/weightRoutes");
 const kickRoutes = require("./routes/kickRoutes"); 
 const path = require("path");
+const resetRoutes = require("./routes/resetRoutes");
+const deleteRoutes = require("./routes/deleteRoutes");
 
 dotenv.config();
 connectDB();
@@ -20,6 +22,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/documents", documentRoutes);
 app.use("/api/weights", weightRoutes);
 app.use("/api/kicks", kickRoutes); 
+app.use("/api/reset", resetRoutes);
+app.use("/api/delete", deleteRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
